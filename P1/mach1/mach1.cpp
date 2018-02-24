cbb05505fe11e42ac824dbaf642111681a5100d0
Niklas Becker <niklasbecker@posteo.de>
2018-02-24 14:56:16 +0100

    improved implementation

--- P1/mach1/mach1.cpp
+++ P1/mach1/mach1.cpp
@@ -5,10 +5,15 @@
 #include <cstdlib>
 
 
+inline double mach(double i)
+{
+	return ((int)i%2==0 ? -1. : 1.)*(4.*pow(1./5., 2*i-1) - pow(1./239., 2*i-1))/(2.*i-1.);
+}
+
 int main(int argc, char** argv)
 {
 	int rank, size;
-	int n = 240;
+	unsigned int n = 1;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -24,45 +29,36 @@ int main(int argc, char** argv)
 		return 0;
 	}
 	
-	double* recv_data = new double[n/size];
-	double sum = 0, start_time, data_prep_duration, scatter_duration, calc_duration;
+	double sum = 0;
+	double start_time, calc_duration;
+	unsigned int left = (unsigned)rank*n/(unsigned)size +1;
+	unsigned int right = ((unsigned)rank+1)*n/(unsigned)size +1;
 	
 	if(rank == 0)
-	{
 		start_time = MPI_Wtime();
-		double* data = new double[n];
-		for(double i = 1; i <= n; i++)
-			data[(int)i] = pow(-1, i-1)*(4.*pow(1./5., 2*i-1) - pow(1./239., 2*i-1))/(2*i-1);
-		data_prep_duration = MPI_Wtime() - start_time;
-		MPI_Scatter(data, (int)n/size, MPI_DOUBLE, recv_data, n/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		scatter_duration = MPI_Wtime() - start_time - data_prep_duration;
-	} else {
-		MPI_Scatter(NULL, (int)n/size, MPI_DOUBLE, recv_data, (int)n/size, MPI_DOUBLE, 0,  MPI_COMM_WORLD); 
-	}
 	
+	for(unsigned int i = left; i <= right; i++)
+		sum += mach(i);
 	
-	
-	for(int i = 0; i < n/size; i++)
-		sum += recv_data[i];
+	//std::cout << rank << ": from " << left << " till " << right << "  - " << sum << std::endl;
 	
 	if(rank == 0)
 	{
-		calc_duration = MPI_Wtime() - start_time - data_prep_duration - scatter_duration;
+		calc_duration = MPI_Wtime() - start_time;
 		double pi;
 		MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 		pi = 4*pi;
 		double duration = MPI_Wtime() - start_time;
 		std::cout << pi << std::endl;
-		std::cout << "Time: " << duration << std::endl;
-		std::cout << "Data Prep: " << data_prep_duration << "\t Scatter: " << scatter_duration << "\t Calc: "<<  calc_duration
-											<< "\tReduce: " << duration - data_prep_duration - scatter_duration - calc_duration  << std::endl;
+		std::cout << "Calc: "<<  calc_duration	<< "\tReduce: " << duration - calc_duration  << std::endl;
 		std::cout << "Error: " << std::abs(M_PI - pi) << std::endl;
 	} else
 	{
 		MPI_Reduce(&sum, NULL, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	}
 	
+	
+	
 	MPI_Finalize();
 	return 0;
 }
-
--- P1/zeta1/zeta1.cpp
+++ P1/zeta1/zeta1.cpp
@@ -5,10 +5,15 @@
 #include <cstdlib>
 
 
+inline double RZ(double i)
+{
+	return 1./pow(i, 2);
+}
+
 int main(int argc, char** argv)
 {
 	int rank, size;
-	int n = 240;
+	unsigned int n = 1;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -24,36 +29,28 @@ int main(int argc, char** argv)
 		return 0;
 	}
 	
-	double* recv_data = new double[n/size];
-	double sum = 0, start_time, data_prep_duration, scatter_duration, calc_duration;
+	double sum = 0;
+	double start_time, calc_duration;
+	unsigned int left = (unsigned)rank*n/(unsigned)size +1;
+	unsigned int right = ((unsigned)rank+1)*n/(unsigned)size +1;
 	
 	if(rank == 0)
-	{
 		start_time = MPI_Wtime();
-		double* data = new double[n];
-		for(double i = 1; i <= n; i++)
-			data[(int)i] = 1./pow(i, 2);
-		data_prep_duration = MPI_Wtime() - start_time;
-		MPI_Scatter(data, (int)n/size, MPI_DOUBLE, recv_data, n/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		scatter_duration = MPI_Wtime() - start_time - data_prep_duration;
-	} else {
-		MPI_Scatter(NULL, (int)n/size, MPI_DOUBLE, recv_data, (int)n/size, MPI_DOUBLE, 0,  MPI_COMM_WORLD); 
-	}
 	
-	for(int i = 0; i < n/size; i++)
-		sum += recv_data[i];
+	for(unsigned int i = left; i <= right; i++)
+		sum += RZ(i);
+	
+	//std::cout << rank << ": from " << left << " till " << right << "  - " << sum << std::endl;
 	
 	if(rank == 0)
 	{
-		calc_duration = MPI_Wtime() - start_time - data_prep_duration - scatter_duration;
+		calc_duration = MPI_Wtime() - start_time;
 		double pi;
 		MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 		pi = sqrt(6*pi);
 		double duration = MPI_Wtime() - start_time;
 		std::cout << pi << std::endl;
-		std::cout << "Time: " << duration << std::endl;
-		std::cout << "Data Prep: " << data_prep_duration << "\t Scatter: " << scatter_duration << "\t Calc: "<<  calc_duration
-											<< "\tReduce: " << duration - data_prep_duration - scatter_duration - calc_duration  << std::endl;
+		std::cout << "Calc: "<<  calc_duration	<< "\tReduce: " << duration - calc_duration  << std::endl;
 		std::cout << "Error: " << std::abs(M_PI - pi) << std::endl;
 	} else
 	{
